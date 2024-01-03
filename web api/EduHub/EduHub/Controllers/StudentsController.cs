03e09e5d2c870ce31575b3dca3f96edadfbfd35a
SameeraDayaratne <85645373+SameeraDayaratne@users.noreply.github.com>
2024-01-03 17:14:38 +0530

    GetStudents Endpoint Implemented

---
+++ web api/EduHub/EduHub/Controllers/StudentsController.cs
@@ -0,0 +1,85 @@
+﻿using EduHub.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EduHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : Controller
+    {
+        private IConfiguration _configuration;
+        
+        public StudentsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        SqlConnection _connection;
+        SqlCommand _command;
+
+
+        [HttpGet]
+        public JsonResult GetStudents()
+        {
+            List<object> students = new List<object>();
+            _connection = new SqlConnection(this._configuration.GetConnectionString("DefaultConnection"));
+
+            string cmd = "SELECT Students.StudentID, Students.FirstName, Students.LastName, Students.ContactPerson, Students.ContactNo, Students.EmailAddress, Students.DateOfBirth, Students.Age,  Classroom.ClassName FROM Students JOIN Classroom ON Students.ClassroomID = Classroom.ClassroomID";
+
+            DataTable dt = new DataTable();
+            _command = new SqlCommand(cmd, _connection);
+            _connection.Open();
+
+            SqlDataAdapter adapter = new SqlDataAdapter(_command);
+            adapter.Fill(dt);
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                var student = new
+                {
+                    StudentId = Convert.ToInt32(dt.Rows[i]["StudentId"]),
+                    FirstName = dt.Rows[i]["FirstName"].ToString(),
+                    LastName = dt.Rows[i]["LastName"].ToString(),
+                    ContactPerson = dt.Rows[i]["ContactPerson"].ToString(),
+                    ContactNo = Convert.ToInt64(dt.Rows[i]["ContactNo"]),
+                    EmailAddress = dt.Rows[i]["EmailAddress"].ToString(),
+                    DateOfBirth = Convert.ToDateTime(dt.Rows[i]["DateOfBirth"]),
+                    ClassName = dt.Rows[i]["ClassName"].ToString(),
+                    Age = Convert.ToInt32(dt.Rows[i]["Age"])
+                };
+                
+
+                students.Add(student);
+            }
+
+            _connection.Close();
+            return Json(students);
+
+
+
+           
+        }
+
+        // POST api/<StudentsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<StudentsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<StudentsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ web api/EduHub/EduHub/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduHub.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        [Required]
+        public string FirstName { get; set; } = String.Empty;
+        [Required]
+        public string LastName { get; set; } = String.Empty;
+        [Required]
+        public string ContactPerson { get; set; } = String.Empty;
+        [Required]
+        public int ContactNo { get; set; }
+        [Required]
+        public string EmailAddress { get; set; } = String.Empty;
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public int ClassroomId { get; set; }
+        [Required]
+        public int Age { get; set; }
+
+    }
+}
